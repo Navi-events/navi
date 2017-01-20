6d6608642bed9793e4145e207ccf5484d66a34a2
Sean Martin <srmartin777@gmail.com>
2017-01-20 17:39:52 -0500

    refactored reducer file

--- app/reducers/events.jsx
+++ app/reducers/events.jsx
@@ -1,15 +1,6 @@
 'use strict';
 import axios from 'axios';
 
-var config = {
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Methdos": "GET, POST, OPTIONS",
-    "Access-Control-Allow-Headers": "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type"
-  }
-};
-
 //*********** Constants ***********:
 
 export const LOAD_ALL_EVENTS = 'LOAD_ALL_EVENTS';
@@ -36,8 +27,8 @@ export const getAllEvents = () => {
 
 
 //*********** Reducer ***********:
-const initialState = { };
-// const initialState = { allEvents: [] };
+
+const initialState = { allEvents: [] };
 
 const eventReducer = (state = initialState, action) => {
   const newState = Object.assign({}, state);
